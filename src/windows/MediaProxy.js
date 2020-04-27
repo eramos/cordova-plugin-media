32ce3a39455d02efcc0aa9abec276d8e2953c699
eramos <eduardo.ramos@openknowledgenetwork.com>
2020-04-27 20:24:25 +0200

    Added support for flac files

--- src/windows/MediaProxy.js
+++ src/windows/MediaProxy.js
@@ -31,7 +31,7 @@ var tempFolderAppDataBasePath = 'ms-appdata:///temp/',
     localFolderFullPath = Windows.Storage.ApplicationData.current.localFolder.path;
 
 var PARAMETER_IS_INCORRECT = -2147024809;
-var SUPPORTED_EXTENSIONS = ['.mp3', '.wma', '.wav', '.cda', '.adx', '.wm', '.m3u', '.wmx', '.m4a'];
+var SUPPORTED_EXTENSIONS = ['.flac', '.mp3', '.wma', '.wav', '.cda', '.adx', '.wm', '.m3u', '.wmx', '.m4a'];
 var SUPPORTED_PREFIXES = ['http', 'https', 'rstp'];
 
 var fsTypes = {
